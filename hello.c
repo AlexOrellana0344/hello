87064e1a376de0bebe162eaed318701cddbc16cf
Alex <aorellana3@huskers.unl.edu>
2022-08-28 22:45:37 +0000

    fixed standard library bug

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+* Basic Hello World Program
+*/
+#include <stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv){
+
+    printf("Hello, I'm Alex Orellana!\n");
+    printf("Major is Computer Engineering!\n");
+
+    return 0;
+}
